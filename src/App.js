f71051234e9236de59517a28e2a3573c383daab5
wanderwisecoder <thissurajneversets@gmail.com>
2023-07-05 14:30:29 +0530

    Accessing Api Key through .env.local file

--- src/App.js
+++ src/App.js
@@ -3,27 +3,134 @@ import './App.css';
 import React, { Component } from 'react';
 import Navbar from './componants/Navbar';
 import News from './componants/News';
-
 import { Routes, Route } from 'react-router-dom';
+import LoadingBar from 'react-top-loading-bar';
 
 export default class App extends Component {
 	pageSize = 20;
+	apiKey = process.env.REACT_APP_NEWS_API;
+	state = {
+		progress: 0,
+	};
+	setProgress = (progress) => {
+		this.setState({ progress: progress });
+	};
 	render() {
 		return (
 			<div>
 				<Navbar />
+				<LoadingBar
+					height={2.5}
+					color='#f11946'
+					progress={this.state.progress}
+				/>
 				<Routes>
-					<Route path='/' element={<News key='general' pageSize={this.pageSize} country='in' category='general' />}></Route>
-					<Route path='/business' element={<News key='business' pageSize={this.pageSize} country='in' category='business' />}></Route>
-					<Route path='/entertainment' element={<News key='entertainment' pageSize={this.pageSize} country='in' category='entertainment' />}></Route>
-					<Route path='/general' element={<News key='general' pageSize={this.pageSize} country='in' category='general' />}></Route>
-					<Route path='/health' element={<News key='health' pageSize={this.pageSize} country='in' category='health' />}></Route>
-					<Route path='/science' element={<News key='science' pageSize={this.pageSize} country='in' category='science' />}></Route>
-					<Route path='/sports' element={<News key='sports' pageSize={this.pageSize} country='in' category='sports' />}></Route>
-					<Route path='/technology' element={<News key='technology' pageSize={this.pageSize} country='in' category='technology' />}></Route>
+					<Route
+						path='/'
+						element={
+							<News
+								setProgress={this.setProgress}
+								apiKey={this.apiKey}
+								key='general'
+								pageSize={this.pageSize}
+								country='in'
+								category='general'
+							/>
+						}
+					></Route>
+					<Route
+						path='/business'
+						element={
+							<News
+								setProgress={this.setProgress}
+								apiKey={this.apiKey}
+								key='business'
+								pageSize={this.pageSize}
+								country='in'
+								category='business'
+							/>
+						}
+					></Route>
+					<Route
+						path='/entertainment'
+						element={
+							<News
+								setProgress={this.setProgress}
+								apiKey={this.apiKey}
+								key='entertainment'
+								pageSize={this.pageSize}
+								country='in'
+								category='entertainment'
+							/>
+						}
+					></Route>
+					<Route
+						path='/general'
+						element={
+							<News
+								setProgress={this.setProgress}
+								apiKey={this.apiKey}
+								key='general'
+								pageSize={this.pageSize}
+								country='in'
+								category='general'
+							/>
+						}
+					></Route>
+					<Route
+						path='/health'
+						element={
+							<News
+								setProgress={this.setProgress}
+								apiKey={this.apiKey}
+								key='health'
+								pageSize={this.pageSize}
+								country='in'
+								category='health'
+							/>
+						}
+					></Route>
+					<Route
+						path='/science'
+						element={
+							<News
+								setProgress={this.setProgress}
+								apiKey={this.apiKey}
+								key='science'
+								pageSize={this.pageSize}
+								country='in'
+								category='science'
+							/>
+						}
+					></Route>
+					<Route
+						path='/sports'
+						element={
+							<News
+								setProgress={this.setProgress}
+								apiKey={this.apiKey}
+								key='sports'
+								pageSize={this.pageSize}
+								country='in'
+								category='sports'
+							/>
+						}
+					></Route>
+					<Route
+						path='/technology'
+						element={
+							<News
+								setProgress={this.setProgress}
+								apiKey={this.apiKey}
+								key='technology'
+								pageSize={this.pageSize}
+								country='in'
+								category='technology'
+							/>
+						}
+					></Route>
 				</Routes>
-
-			</div >
-		)
+			</div>
+		);
 	}
 }
